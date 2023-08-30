d4213c41023aec40c4a357758fcf653efff3c22c
FlorinBejera2002 <florinpetru0306@gmail.com>
2023-08-30 22:28:29 +0300

    initial commit

--- src/components/ Profile/Profile.jsx
+++ src/components/ Profile/Profile.jsx
@@ -1,47 +1,62 @@
-export default function Profile({
-    username,
-    tag,
-    location,
-    avatar,
-    stats: { followers, views, likes },
-}) {
-    return (
-        <ProfileCard>
-            <Description>
-                <AvatarWrapper>
-                    <Avatar src={avatar} alt="User avatar" />
-                </AvatarWrapper>
-                <Name>{username}</Name>
-                <Tag>@{tag}</Tag>
-                <Location>{location}</Location>
-            </Description>
+import PropTypes from 'prop-types';
+import {
+  ProfileCard,
+  Description,
+  AvatarWrapper,
+  Avatar,
+  Name,
+  Tag,
+  Location,
+  StatsWrapper,
+  StatsItem,
+  Label,
+  Quantity,
+} from './Profile.styled';
 
-            <StatsWrapper>
-                <StatsItem>
-                    <Label>Followers</Label>
-                    <Quantity>{followers}</Quantity>
-                </StatsItem>
-                <StatsItem>
-                    <Label>Views</Label>
-                    <Quantity>{views}</Quantity>
-                </StatsItem>
-                <StatsItem>
-                    <Label>Likes</Label>
-                    <Quantity>{likes}</Quantity>
-                </StatsItem>
-            </StatsWrapper>
-        </ProfileCard>
-    );
+export default function Profile({
+  username,
+  tag,
+  location,
+  avatar,
+  stats: { followers, views, likes },
+}) {
+  return (
+    <ProfileCard>
+      <Description>
+        <AvatarWrapper>
+          <Avatar src={avatar} alt="User avatar" />
+        </AvatarWrapper>
+        <Name>{username}</Name>
+        <Tag>@{tag}</Tag>
+        <Location>{location}</Location>
+      </Description>
+
+      <StatsWrapper>
+        <StatsItem>
+          <Label>Followers</Label>
+          <Quantity>{followers}</Quantity>
+        </StatsItem>
+        <StatsItem>
+          <Label>Views</Label>
+          <Quantity>{views}</Quantity>
+        </StatsItem>
+        <StatsItem>
+          <Label>Likes</Label>
+          <Quantity>{likes}</Quantity>
+        </StatsItem>
+      </StatsWrapper>
+    </ProfileCard>
+  );
 }
 
 Profile.propTypes = {
-    username: PropTypes.string.isRequired,
-    tag: PropTypes.string.isRequired,
-    location: PropTypes.string.isRequired,
-    avatar: PropTypes.string.isRequired,
-    stats: PropTypes.shape({
-        followers: PropTypes.number.isRequired,
-        views: PropTypes.number.isRequired,
-        likes: PropTypes.number.isRequired,
-    }),
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
 };
