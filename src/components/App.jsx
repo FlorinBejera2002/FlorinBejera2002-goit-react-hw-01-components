c503ac4282d038247aaa00d00b802d101c592554
FlorinBejera2002 <florinpetru0306@gmail.com>
2023-08-31 20:48:08 +0300

    finis first react homework

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
-import Profile from './Profile/Profile';
+ import Profile from './ Profile/Profile';
 import Statistics from './Statistics/Statistics';
 import FriendList from './FriendList/FriendList';
 import TransactionHistory from './TransactionHistory/TransactionHistory';
-import user from '../data/user.json';
-import data from '../data/data.json';
-import friends from '../data/friends.json';
-import transactions from '../data/transactions.json';
+import user from '../data/user';
+import data from '../data/data';
+import friends from '../data/friends';
+import transactions from '../data/transactions';
 
 export function App() {
   document.title = 'Homework 1';
